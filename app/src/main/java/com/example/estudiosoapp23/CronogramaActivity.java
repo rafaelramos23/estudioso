77774b33d5ada928e087369de520278a3f6a7f2f
rafaelramos23 <raffaellramos@gmail.com>
2020-11-12 23:01:34 -0300

    versão inicial

---
+++ app/src/main/java/com/example/estudiosoapp23/CronogramaActivity.java
@@ -0,0 +1,14 @@
+package com.example.estudiosoapp23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CronogramaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cronograma);
+    }
+}
---
+++ app/src/main/java/com/example/estudiosoapp23/NovaSessaoActivity.java
@@ -0,0 +1,14 @@
+package com.example.estudiosoapp23;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NovaSessaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nova_sessao);
+    }
+}
